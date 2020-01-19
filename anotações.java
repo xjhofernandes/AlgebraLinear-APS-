ae7cd8f5b9282cbf71ae3336cbbbbdfb08efc24e
Jonathan Fernandes <46504678+xjhofernandes@users.noreply.github.com>
2020-01-19 11:12:53 -0300

    Anotações - Java

---
+++ anotações.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+public class APP {
+	public static void main(String[] args) {
+	LocalDate today = LocalDate.now();                          //Data Atual
+	LocalDate birthday = LocalDate.of(1995, Month.NOVEMBER, 7);  //Data do depósito
+	
+	Period p = Period.between(birthday, today);		
+	
+	System.out.println(p.getDays());
+	System.out.println(p.getYears());
+	System.out.println(p.toTotalMonths()); //Retornando a quantidade de meses com a precisão correta. 
+	}	
+}
